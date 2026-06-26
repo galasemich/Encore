218999777782cc640a12f5a22e5ef7c2eb207c5b
gala semich alvarez <gala.semich@gmail.com>
2026-06-26 08:54:50 -0300

    sigo haciendo pruebas

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import http from "http"
 
 const server = http.createServer((req, res)=> {
-    res.writeHead(200, {'Content-type': 'text-plain'})
+    res.writeHead(200, {'Content-type': 'text-plain',
+        'X-Powered-By': 'node.js'
+    })
     // Acá podemos desestructurar el objeto req, que cuenta con las propiedades url y method. También podríamos hacer algo como
     // url = req.url
-    // method = req.method
-    const { url, method } = req
+    // method = req.method,
+    // pero la desestructuración nos ahorra líneas de código. 
+    const { url, method, headers } = req
+    
     res.end(`Hiciste una request ${method} en la URL ${url}`)
 })
 
