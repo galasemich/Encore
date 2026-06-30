cd8662ec7764d939ffaec6ed3c1cab04fef524e7
gala semich alvarez <gala.semich@gmail.com>
2026-06-30 16:56:34 -0300

    definí datos de prueba para mostrar el funcionamiento

---
+++ data.js
@@ -0,0 +1,13 @@
+const usuarios = [
+    {nombreUsuario: "galapha", email: "gala@mail.com"},
+    {nombreUsuario: "juanp", email: "juanp@mail.com"},
+    {nombreUsuario: "mariag", email: "mariag@mail.com"}
+]
+
+const tareas = [
+    {nombre: "Hacer la compra", categoria: "Urgente", usuario: "galapha"},
+    {nombre: "Arreglar calefón", categoria: "Puede esperar", usuario: "juanp"},
+    {nombre: "Terminar monografía", categoria: "Urgente", usuario: "mariag"}
+]
+
+export { usuarios, tareas }
