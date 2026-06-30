6460db702666498243d34f89a1199d91bd8c46ae
gala semich alvarez <gala.semich@gmail.com>
2026-06-30 16:56:18 -0300

    definí tres handlers sencillos para ilustrar el proceso del framework

---
+++ handlers.js
@@ -0,0 +1,21 @@
+import { usuarios, tareas } from "./data.js"
+
+function traerUsuarios(req, res) {
+    res.writeHead(200, {
+        "Content-type": "application/json"
+    })
+    res.end(JSON.stringify(usuarios, null, 2))
+}
+
+function traerTareas(req, res) {
+    res.writeHead(200, {
+        "Content-type": "application/json"
+    })
+    res.end(JSON.stringify(tareas, null, 2))
+}
+
+function inicio(req, res) {
+    res.end(JSON.stringify({mensaje: "Bienvenidos a mi servidor creado con Encore."}), null, 2)
+}
+
+export default { traerTareas, traerUsuarios, inicio }
