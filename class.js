c51b26edf6f7daa3663a967e01e8e87f9c9192dc
gala semich alvarez <gala.semich@gmail.com>
2026-06-30 16:57:19 -0300

    terminé de corregir método que levanta el servidor

--- class.js
+++ class.js
@@ -18,27 +18,17 @@ class App {
 
     levantarServidor() {
         const servidor = http.createServer((req, res) => {
-            res.end("Hola, mundo.")
+            const handler = this.verificarRuta(req.url, req.method)
+            if (handler) {
+                handler(req, res)
+            } else {
+                res.writeHead(404)
+                res.end(JSON.stringify({mensaje: "Ruta no encontrada."}), null, 2)
+            }
         })
         
         servidor.listen(puerto, () => {console.log(`Servidor corriendo en ${puerto}.`)})
     }
-
-    //listen(puerto, callback) {
-    // Esta función define el servidor y escucha. 
-        // const servidor = http.createServer((req, res) => {
-            // const handler = this.verificarRuta(req.url, req.method)
-            // if (handler) {
-            //     res.writeHead(200)
-            //     handler(req, res)
-            // } else {
-            //     res.writeHead(404)
-            //     res.end("Ruta no encontrada.")
-            // }
-        // })
-
-        //servidor.listen(puerto, callback)
-    //}
 
     // Definimos una función que verifica si la ruta existe con método. 
     verificarRuta(ruta, metodo) {
