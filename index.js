21a3554c6a15adb63804151602ceb23d55d85fdb
gala semich alvarez <gala.semich@gmail.com>
2026-06-29 21:11:20 -0300

    comenté el registro de rutas momentáneamente

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ import http from "http"
 import { App } from "./class.js"
 
 const app = new App()
-const puerto = 3000
+app.levantarServidor()
+// app.regitrarRuta("/", inicio, "GET")
+// app.regitrarRuta("/productos", productos, "GET")
+// app.regitrarRuta("/usuarios", usuarios, "GET")
 
-app.regitrarRuta("/", inicio, "GET")
-app.regitrarRuta("/productos", productos, "GET")
-app.regitrarRuta("/usuarios", usuarios, "GET")
-
-app.listen(puerto, () => {`Servidor corriendo en ${puerto}.`})
+// app.listen(puerto, () => {`Servidor corriendo en ${puerto}.`})
