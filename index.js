ae68e0d70f74454e88bcd4d54a80cd1e8e46ccea
gala semich alvarez <gala.semich@gmail.com>
2026-07-28 11:20:27 -0300

    registré el middleware para setear header y agregué el de autenticación a una de las rutas

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ import middlewares from "./middlewares.js"
 const app = new App()
 
 app.registrarRuta("/", handlers.inicio, "GET")
-app.registrarRuta("/tareas", handlers.traerTareas, "GET")
+app.registrarRuta("/tareas", handlers.traerTareas, "GET", [middlewares.autenticacion])
 app.registrarRuta("/usuarios", handlers.traerUsuarios, "GET")
 app.registrarRuta("/tarea", handlers.nuevaTarea, "POST")
 
 app.registrarMiddleware(middlewares.middlewareMetodo)
+app.registrarMiddleware(middlewares.setearHeader)
 
 app.levantarServidor()
 
