1191504bc7e382cab433cba1356fb5f64511d54c
gala semich alvarez <gala.semich@gmail.com>
2026-08-02 15:32:17 -0300

    agregué función para traer un usuario por nombre

--- handlers.js
+++ handlers.js
@@ -14,6 +14,21 @@ function traerTareas(req, res) {
     res.end(JSON.stringify(tareas, null, 2))
 }
 
+function traerUsuario(req, res) {
+    res.writeHead(200, {
+        "Content-type": "application/json"
+    })
+
+    const usuarioBuscar = req.params
+    const usuario = usuarios.filter((usuario) => usuario.nombreUsuario === usuarioBuscar.nombre)
+    
+    if (usuario) {
+        res.end(JSON.stringify(usuario, null, 2))
+    } else {
+        res.end(JSON.stringify({mensaje: "No se encontró ningún usuario con ese nombre."}))
+    }
+}
+
 function inicio(req, res) {
     res.end(JSON.stringify({mensaje: "Bienvenidos a mi servidor creado con Encore."}), null, 2)
 }
@@ -24,4 +39,4 @@ function nuevaTarea(req, res) {
     res.end(JSON.stringify({mensaje: "Tarea agregada correctamente."}, null, 2))
 }
 
-export default { traerTareas, traerUsuarios, inicio, nuevaTarea }
+export default { traerTareas, traerUsuarios, inicio, nuevaTarea, traerUsuario }
