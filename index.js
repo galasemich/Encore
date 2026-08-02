e58ae459b74a53ffb0c4310170fe50b83e3a05f2
gala semich alvarez <gala.semich@gmail.com>
2026-08-02 15:32:51 -0300

    registré endpoint para traer usuario por nombre

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.registrarRuta("/", handlers.inicio, "GET")
 app.registrarRuta("/tareas", handlers.traerTareas, "GET", [middlewares.autenticacion])
 app.registrarRuta("/usuarios", handlers.traerUsuarios, "GET")
 app.registrarRuta("/tarea", handlers.nuevaTarea, "POST")
+app.registrarRuta("/usuario/:nombre", handlers.traerUsuario, "GET")
 
 app.registrarMiddleware(middlewares.middlewareMetodo)
 app.registrarMiddleware(middlewares.setearHeader)
