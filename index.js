f0cfb14214b3a667766dd18906347d0d6ca550dc
gala semich alvarez <gala.semich@gmail.com>
2026-08-03 11:31:46 -0300

    agregué rutas put y delete y cambié el parámetro de ruta de /usuario/:nombre/ por :id

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ import middlewares from "./middlewares.js"
 
 const app = new App()
 
+// Rutas GET
 app.registrarRuta("/", handlers.inicio, "GET")
 app.registrarRuta("/tareas", handlers.traerTareas, "GET", [middlewares.autenticacion])
 app.registrarRuta("/usuarios", handlers.traerUsuarios, "GET")
-app.registrarRuta("/tarea", handlers.nuevaTarea, "POST")
-app.registrarRuta("/usuario/:nombre", handlers.traerUsuario, "GET")
+app.registrarRuta("/usuario/:id", handlers.traerUsuario, "GET")
 
+// Rutas POST
+app.registrarRuta("/tarea", handlers.nuevaTarea, "POST")
+
+// Rutas DELETE
+app.registrarRuta("/tarea/:nombre", handlers.eliminarTarea, "DELETE")
+
+// Rutas PUT
+app.registrarRuta("/tarea/:categoria", handlers.editarTarea, "PUT")
+
+// Middlewares
 app.registrarMiddleware(middlewares.middlewareMetodo)
 app.registrarMiddleware(middlewares.setearHeader)
 app.registrarMiddleware(middlewares.parsearBody)
