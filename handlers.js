31b51217a4f14523bd286d2923350fb45dbf2bcf
gala semich alvarez <gala.semich@gmail.com>
2026-08-03 18:04:05 -0300

    agregué función editarUsuario

--- handlers.js
+++ handlers.js
@@ -12,7 +12,7 @@ async function traerUsuarios(req, res) {
     const consulta = "SELECT * FROM usuarios"
     const resultado = await conexion.execute(consulta)
 
-    res.end(JSON.stringify(usuarios, null, 2))
+    res.end(JSON.stringify(resultado[0], null, 2))
 }
 
 async function traerTareas(req, res) {
@@ -31,9 +31,13 @@ async function traerUsuario(req, res) {
         "Content-type": "application/json"
     })
 
-    const parametros = req.params
-    const idUsuario = parametros.id
-    console.log(idUsuario)
+    try {
+        const { id } = req.params
+    } catch (error) {
+        console.log("Error en la desestructuración:", error)
+        res.end(JSON.stringify({mensaje: "El id del usuario no es válido."}))
+    }
+    
     const consulta = "SELECT * FROM usuarios WHERE id = ?"
 
     try {
@@ -44,7 +48,7 @@ async function traerUsuario(req, res) {
             res.end(JSON.stringify({mensaje: `No se encontró el usuario con id = ${id}.`}))
         }
     } catch (error) {
-        console.log(error)
+        console.log("Error en función traerUsuarios:", error)
         res.end(JSON.stringify({mensaje: "No se pudo completar la solicitud."}))
     }
 }
@@ -61,7 +65,7 @@ async function nuevaTarea(req, res) {
         const resultado = await conexion.execute(consulta, [nombre, categoria, id_usuario])
         res.end(JSON.stringify({mensaje: "Tarea agregada correctamente."}, null, 2))
     } catch (error) {
-        console.log(error)
+        console.log("Error en función nuevaTarea:", error)
         res.end(JSON.stringify({mensaje: "No se pudo agregar la tarea a la base de datos."}))
     }
 }
@@ -75,14 +79,45 @@ function eliminarTarea(req, res) {
         if (!tarea.nombre === tareaEliminar.nombre) {
             res.end(JSON.stringify({mensaje: "No se encontró la tarea solicitada."}))
         } else {
-
+            
         }
-
     })
 }
 
-function editarTarea(req, res) {
+async function editarUsuario(req, res) {
+   res.writeHead(200, {
+        "Content-type": "application/json"
+    })
 
+    const idUsuario = req.params.id
+
+    for (const par of Object.entries(req.body)) {
+        const columna = par[0]
+        const dato = par[1]
+
+        const consulta = `UPDATE usuarios SET ${columna} = ? WHERE id = ?`
+        
+        try {
+            const resultado = await conexion.execute(consulta, [dato, idUsuario])
+
+            if (resultado[0]) {
+                res.end(JSON.stringify({mensaje: "Usuario actualizad correctamente."}))
+            } else {
+                res.end(JSON.stringify({mensaje: `No se encontró la tarea con id = ${id}.`}))
+            }
+        } catch (error) {
+            console.log("Error en la consulta a la base de datos:", error)
+            res.end(JSON.stringify({mensaje: "No se pudo completar la solicitud."}))
+        }
+    }
 }
 
-export default { traerTareas, traerUsuarios, inicio, nuevaTarea, traerUsuario, eliminarTarea, editarTarea }
+export default { 
+    traerTareas, 
+    traerUsuarios, 
+    inicio, 
+    nuevaTarea, 
+    traerUsuario, 
+    eliminarTarea, 
+    editarUsuario 
+}
